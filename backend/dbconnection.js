9626431475b2bbdb331673fb0269b79becbc369d
Máté <szulicsmate@Mate-MacBook-Pro.local>
2026-05-10 17:58:26 +0200

    Ideiglenes mentés a merge előtt

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,4 +1,6 @@
 import { DatabaseSync } from 'node:sqlite';
+import { createHmac } from 'node:crypto';
+import e from 'express';
 
 const database = new DatabaseSync('webappdb.sqlite');
 
@@ -23,6 +25,17 @@ function ensureExists() {
         products TEXT
     ) STRICT
     `);
+
+    // user
+    database.exec(`
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        mail TEXT,
+        psw TEXT,
+        cartid INTEGER
+    ) STRICT
+    `);
 }
 
 export function getAllProducts() {
@@ -34,6 +47,65 @@ export function getAllProducts() {
 export function addProduct(name, desc, longdesc, price, purchasable = 1, flags = []) {
     ensureExists();
     const insert = database.prepare('INSERT INTO products (name, desc, longdesc, price, purchasable, flags) VALUES (?, ?, ?, ?, ?, ?)');
-
     insert.run(name, desc, longdesc, price, purchasable, JSON.stringify(flags));
+}
+
+export function hasProductName(name) {
+    ensureExists();
+    const insert = database.prepare('SELECT id FROM products WHERE products.name == ? ');
+    return insert.get(name) != undefined;
+}
+
+function hashPsw(psw) {
+    // salting and hashing password
+    const hmac = createHmac('sha256', 'MyFancySecret');
+    hmac.update(psw + "SALTHERE++++!+3463564527 bgrtzh56u");
+    return hmac.digest('hex');
+}
+
+export function registerUser(name, mail, psw) {
+    ensureExists();
+    // check for user login, some people used to register and login more times (mahaps frogot to registered?)
+    if (loginUser(mail, psw))
+    {
+        // code 1 means user already registered, log the user in!
+        return 1;
+    }
+    // user already has mail in the db.
+    else if (hasUser(mail))
+    {
+        // code 2 means user already registered but the psw is different!
+        return 2;
+    }
+
+    psw = hashPsw(psw);
+    const insert = database.prepare('INSERT INTO users (name, mail, psw, cartid) VALUES (?, ?, ?, 0)');
+    insert.run(name, mail, psw);
+    return 0; // 0 as a "ok" resposnse
+}
+
+// returns true or false if user is exists.
+export function loginUser(mail, psw) {
+    psw = hashPsw(psw);
+    ensureExists();
+    const insert = database.prepare('SELECT id FROM users WHERE users.mail == ? AND users.psw == ?');
+    return insert.get(mail, psw) != undefined;
+}
+
+function hasUser(mail) {
+    ensureExists();
+    const select = database.prepare('SELECT id FROM users WHERE users.mail == ?');
+    return select.get(mail) != undefined;
+}
+
+export function deleteUser(mail) {
+    ensureExists();
+    const del = database.prepare('DELETE FROM users WHERE users.mail == ?');
+    del.run(mail);
+}
+
+export function mergeCart(mail, cartId) {
+    ensureExists();
+    const update = database.prepare('UPDATE users SET users.cartid = ? WHERE users.mail == ?');
+    update.run(cartId, mail);
 }
--- backend/test.js
+++ backend/test.js
@@ -1,13 +1,26 @@
-import { addProduct } from './dbconnection.js';
+import { addProduct, registerUser, loginUser, deleteUser, hasProductName } from './dbconnection.js';
 
 // Név, rövid leírás, hosszú leírás, ár, vásárolható-e (1/0), flagek
-addProduct(
-    "Gamer Egér 2026", 
-    "Szupergyors és világít és még meg is vásárolható!", 
-    "Ez a legújabb modell, 25000 DPI felbontással.", 
-    20000, 
-    0, 
-    []
-);
+if (!hasProductName("Gamer Egér 2026"))
+{
+    addProduct(
+        "Gamer Egér 2026", 
+        "Szupergyors és világít és még meg is vásárolható!", 
+        "Ez a legújabb modell, 25000 DPI felbontással.", 
+        20000, 
+        0, 
+        []
+    );
 
-console.log("Teszt termék(ek) hozzáadva az adatbázishoz!");
+    console.log("Teszt termék(ek) hozzáadva az adatbázishoz!");
+}
+else
+{
+    console.log("Teszt termék(ek) hozzáadva az adatbázishoz!");
+}
+
+// login test
+console.log(registerUser("test", "mate", "psw!!"));
+console.log(loginUser("mate", "psw!!"));
+console.log(loginUser("mate2", "psw!!"));
+deleteUser("mate");
