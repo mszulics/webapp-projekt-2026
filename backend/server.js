597476794a1c98155608273625e073f64da3a130
Máté <szulicsmate@Mate-MacBook-Pro.local>
2026-05-10 17:48:29 +0200

    cors és type:module hozzáadása a backendhez

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { getAllProducts, loginUser, registerUser } from './dbconnection.js'
 const app = express()
 const port = 7777
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
